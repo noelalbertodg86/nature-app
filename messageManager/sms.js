c371569184ce7735813de7e72d0d3a9843ca7332
Noel Diaz <noel@Noels-MBP.cpe.satnet.net>
2020-05-05 21:02:05 -0500

    adding sms message

--- messageManager/sms.js
+++ messageManager/sms.js
@@ -1,13 +1,49 @@
 const Nexmo = require("nexmo");
 const Message = require("../models").Message;
 const Appointment = require("../models").Appointment;
+const request = require("request");
 
 const nexmo = new Nexmo({
   apiKey: "7bced3b7",
   apiSecret: "wZ1YTyGpwEYI2m5c"
 });
 
+async function sendOnlineSms(messageType, appointmentId) {
+  var smsData = await getSmsData(messageType, appointmentId);
+  var from = smsData.from;
+  var to = smsData.to;
+  var text = smsData.text;
+  nexmo.message.sendSms(from, to, text);
+  console.log("... SMS enviado", to, text);
+}
+
 async function sendSms(messageType, appointmentId) {
+  var smsData = await getSmsData(messageType, appointmentId);
+  console.log("sms data", smsData);
+  var smsUser = "noel.diaz";
+  var smsPassword = "Da14Ca16";
+  var to = "+" + smsData.to;
+  var messageText = smsData.text;
+  var url = `http://192.168.1.133:8090/SendSMS?username=${smsUser}&password=${smsPassword}&phone=${to}&message=${messageText}`;
+
+  console.log("URL..", url);
+  request.get(url, { timeout: 30000 }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    if (res.statusCode !== 200) {
+      console.log("... The local service for send SMS is down");
+      return;
+    }
+    var info = JSON.parse(body);
+    //console.log("body", body.url);
+    if (info.status === "200") {
+      console.log("Mensaje enviado exitosamente: ", info.phone, messageText);
+    }
+  });
+}
+
+async function getSmsData(messageType, appointmentId) {
   const smsMessage = await Message.findOne({
     where: {
       canal: "SMS",
@@ -32,8 +68,9 @@ async function sendSms(messageType, appointmentId) {
   var from = "NATURE Centro Medico";
   var to = "593" + appointmentModel.client.cellPhone.substr(-9, 9);
   var text = smsMessage.message;
-  nexmo.message.sendSms(from, to, text);
-  console.log("... SMS enviado", to, text);
+
+  return { from, to, text };
 }
 
+exports.sendOnlineSms = sendOnlineSms;
 exports.sendSms = sendSms;
