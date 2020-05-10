1abb72e0f8211ebd2b956daf116ba6ec56d6f69e
Noel Diaz <noel@Noels-MBP.cpe.satnet.net>
2020-05-10 16:33:08 -0500

    set ip reader

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nature-app
+
+Se desplego el sistema con heroku en:
+https://shrouded-tundra-99134.herokuapp.com/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var ip = require("ip");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express: " + ip.address() });
+  console.log(">>>>> IP", ip.address());
 });
 
 module.exports = router;
